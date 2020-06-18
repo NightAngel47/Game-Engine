699adf7b6365bd44a865c604a978eb074bf3e9bb
Steven <NightAngel47@users.noreply.github.com>
2020-06-18 15:40:45 -0500

    Implemented Window Events

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -1,19 +1,28 @@
 #include "enginepch.h"
 #include "Application.h"
 
-#include "Engine/Events/ApplicationEvent.h"
 #include "Engine/Log.h"
 
 namespace Engine
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
 	
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallBack(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		
+		ENGINE_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -23,5 +32,10 @@ namespace Engine
 			m_Window->OnUpdate();
 		}
 	}
-	
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
 }
