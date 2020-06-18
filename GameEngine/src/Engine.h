cef70b45bfe802804c63f086fdb0e02eb0c388e2
Steven <NightAngel47@users.noreply.github.com>
2020-06-17 19:48:23 -0500

    Entry Point

---
+++ GameEngine/src/Engine.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Game Engine applications
+
+#include "Engine/Application.h"
+
+// ---Entry Point----------------------------
+#include "Engine/EntryPoint.h"
+// ------------------------------------------
