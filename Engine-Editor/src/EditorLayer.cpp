474bef9f9c4887470cfb449a9c0ef00bbcad15bc
Steven <NightAngel47@users.noreply.github.com>
2020-10-20 19:15:17 -0500

    Fully Functional Transform Component
    Transfrom component now has position, rotation, and scale displayed properly.

--- Engine-Editor/src/EditorLayer.cpp
+++ Engine-Editor/src/EditorLayer.cpp
@@ -40,12 +40,12 @@ namespace Engine
 		cc.Primary = false;
 
 		class CameraController : public ScriptableEntity
-		{
+		{	
 		public:
 			void OnCreate()
 			{
-				auto& transform = GetComponent<TransformComponent>().Transform;
-				transform[3][0] = rand() % 10 - 5.0f;
+				auto& position = GetComponent<TransformComponent>().Position;
+				position.x = rand() % 10 - 5.0f;
 			}
 
 			void OnDestroy()
@@ -55,17 +55,17 @@ namespace Engine
 
 			void OnUpdate(Timestep ts)
 			{
-				auto& transform = GetComponent<TransformComponent>().Transform;
+				auto& position = GetComponent<TransformComponent>().Position;
 				float speed = 5.0f;
 				
 				if (Input::IsKeyPressed(KeyCode::A))
-					transform[3][0] -= speed * ts;
+					position.x -= speed * ts;
 				if (Input::IsKeyPressed(KeyCode::D))
-					transform[3][0] += speed * ts;
+					position.x += speed * ts;
 				if (Input::IsKeyPressed(KeyCode::W))
-					transform[3][1] += speed * ts;
+					position.y += speed * ts;
 				if (Input::IsKeyPressed(KeyCode::S))
-					transform[3][1] -= speed * ts;
+					position.y -= speed * ts;
 			}
 		};
 
