49d81530539dd8a256477b174b6fe8820f4212e8
Steven Drovie <NightAngel47@users.noreply.github.com>
2021-02-24 16:20:53 -0600

    Updated Game-Engine Premake files

---
+++ Engine-Editor/premake5.lua
@@ -0,0 +1,47 @@
+project "Engine-Editor"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/Hazel/vendor/spdlog/include",
+		"%{wks.location}/Hazel/src",
+		"%{wks.location}/Hazel/vendor",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.entt}",
+		"%{IncludeDir.ImGuizmo}"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		runtime "Release"
+		optimize "on"
--- premake5.lua
+++ premake5.lua
@@ -1,3 +1,5 @@
+include "./vendor/premake/premake_customization/solution_items.lua"
+
 workspace "GameEngine"
 	architecture "x86_64"
 	startproject "Engine-Editor"
@@ -9,6 +11,11 @@ workspace "GameEngine"
 		"Dist"
 	}
 
+	solution_items
+	{
+		".editorconfig"
+	}
+
 	flags
 	{
 		"MultiProcessorCompile"
@@ -18,215 +25,23 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 -- Include directories relative to root folder (solution directory)
 IncludeDir = {}
-IncludeDir["GLFW"] = "GameEngine/vendor/GLFW/include"
-IncludeDir["Glad"] = "GameEngine/vendor/Glad/include"
-IncludeDir["ImGui"] = "GameEngine/vendor/imgui"
-IncludeDir["glm"] = "GameEngine/vendor/glm"
-IncludeDir["stb_image"] = "GameEngine/vendor/stb_image"
-IncludeDir["entt"] = "GameEngine/vendor/entt/include"
-IncludeDir["yaml_cpp"] = "GameEngine/vendor/yaml-cpp/include"
-IncludeDir["ImGuizmo"] = "GameEngine/vendor/ImGuizmo"
+IncludeDir["GLFW"] = "%{wks.location}/GameEngine/vendor/GLFW/include"
+IncludeDir["Glad"] = "%{wks.location}/GameEngine/vendor/Glad/include"
+IncludeDir["ImGui"] = "%{wks.location}/GameEngine/vendor/imgui"
+IncludeDir["glm"] = "%{wks.location}/GameEngine/vendor/glm"
+IncludeDir["stb_image"] = "%{wks.location}/GameEngine/vendor/stb_image"
+IncludeDir["entt"] = "%{wks.location}/GameEngine/vendor/entt/include"
+IncludeDir["yaml_cpp"] = "%{wks.location}/GameEngine/vendor/yaml-cpp/include"
+IncludeDir["ImGuizmo"] = "%{wks.location}/GameEngine/vendor/ImGuizmo"
 
 group "Dependencies"
+	include "vendor/premake"
 	include "GameEngine/vendor/GLFW"
 	include "GameEngine/vendor/Glad"
 	include "GameEngine/vendor/imgui"
 	include "GameEngine/vendor/yaml-cpp"
-
 group ""
 
-project "GameEngine"
-	location "GameEngine"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "enginepch.h"
-	pchsource "GameEngine/src/enginepch.cpp"
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp",
-		"%{prj.name}/vendor/stb_image/**.h",
-		"%{prj.name}/vendor/stb_image/**.cpp",
-		"%{prj.name}/vendor/glm/glm/**.hpp",
-		"%{prj.name}/vendor/glm/glm/**.inl",
-
-		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.h",
-		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.cpp"
-	}
-
-	defines
-	{
-		"_CRT_SECURE_NO_WARNINGS",
-		"GLFW_INCLUDE_NONE",
-		"_SILENCE_CXX17_RESULT_OF_DEPRECATION_WARNING"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.yaml_cpp}",
-		"%{IncludeDir.ImGuizmo}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"yaml-cpp",
-		"opengl32.lib"
-	}
-
-	filter "files:GameEngine/vendor/ImGuizmo/**.cpp"
-	flags { "NoPCH" }
-
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"GLFW_INCLUDE_NONE"
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		optimize "on"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"GameEngine/vendor/spdlog/include",
-		"GameEngine/src",
-		"GameEngine/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}"
-	}
-
-	links
-	{
-		"GameEngine"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		optimize "on"
-
-
-project "Engine-Editor"
-	location "Engine-Editor"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "on"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	defines
-	{
-		"_SILENCE_CXX17_RESULT_OF_DEPRECATION_WARNING"
-	}
-
-	includedirs
-	{
-		"GameEngine/vendor/spdlog/include",
-		"GameEngine/src",
-		"GameEngine/vendor",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.entt}",
-		"%{IncludeDir.ImGuizmo}"
-	}
-
-	links
-	{
-		"GameEngine"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		runtime "Release"
-		optimize "on"
+include "GameEngine"
+include "Sandbox"
+include "Engine-Editor"
