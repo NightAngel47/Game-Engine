e8044707463bb902f1b1a184a79f08c6366f2c2b
Steven Drovie <NightAngel47@users.noreply.github.com>
2022-07-09 17:10:11 -0500

    Physics Simulation

--- GameEngine/src/Engine/Scene/Scene.cpp
+++ GameEngine/src/Engine/Scene/Scene.cpp
@@ -124,153 +124,38 @@ namespace Engine
 	void Scene::OnRuntimeStart()
 	{
 		// Create Physics Objects
-		{
-			m_PhysicsWorld = new b2World({ 0.0f, -9.8f });
-
-			auto view = m_Registry.view<Rigidbody2DComponent>();
-			for (auto e : view)
-			{
-				Entity entity = { e, this };
-				auto& transform = entity.GetComponent<TransformComponent>();
-				auto& rb2d = entity.GetComponent<Rigidbody2DComponent>();
-
-				b2BodyDef bodyDef;
-				bodyDef.type = Rigidbody2DTypeToBox2DBodyType(rb2d.Type);
-				bodyDef.position.Set(transform.Position.x, transform.Position.y);
-				bodyDef.angle = transform.Rotation.z;
-
-				b2Body* body = m_PhysicsWorld->CreateBody(&bodyDef);
-
-				body->SetFixedRotation(rb2d.FixedRotation);
-				rb2d.RuntimeBody = body;
-
-				if (entity.HasComponent<BoxCollider2DComponent>())
-				{
-					auto& bc2d = entity.GetComponent<BoxCollider2DComponent>();
-
-					b2PolygonShape boxShape;
-					boxShape.SetAsBox(bc2d.Size.x * transform.Scale.x, bc2d.Size.y * transform.Scale.y, b2Vec2(bc2d.Offset.x, bc2d.Offset.y), 0);
-
-					b2FixtureDef fixtureDef;
-					fixtureDef.shape = &boxShape;
-					fixtureDef.density = bc2d.Density;
-					fixtureDef.friction = bc2d.Friction;
-					fixtureDef.restitution = bc2d.Restitution;
-					fixtureDef.restitutionThreshold = bc2d.RestitutionThreshold;
-
-					body->CreateFixture(&fixtureDef);
-				}
-
-				if (entity.HasComponent<CircleCollider2DComponent>())
-				{
-					auto& cc2d = entity.GetComponent<CircleCollider2DComponent>();
-
-					b2CircleShape circleShape;
-					circleShape.m_p.Set(cc2d.Offset.x, cc2d.Offset.y);
-					circleShape.m_radius = transform.Scale.x * cc2d.Radius;
-
-					b2FixtureDef fixtureDef;
-					fixtureDef.shape = &circleShape;
-					fixtureDef.density = cc2d.Density;
-					fixtureDef.friction = cc2d.Friction;
-					fixtureDef.restitution = cc2d.Restitution;
-					fixtureDef.restitutionThreshold = cc2d.RestitutionThreshold;
-
-					body->CreateFixture(&fixtureDef);
-				}
-			}
-		}
+		OnPhysics2DStart();
 
 		// Start Scripts
-		{
-			m_Registry.view<NativeScriptComponent>().each([=](auto entity, auto& nsc)
-			{
-				if (!nsc.Instance)
-				{
-					nsc.Instance = nsc.InstantiateScript();
-					nsc.Instance->m_Entity = Entity{ entity, this };
-					nsc.Instance->OnCreate();
-				}
-			});
+		OnScriptsStart();
+	}
 
-			m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
-			{
-				if (!sc.scriptInstatiated)
-				{
-					sc.InstantiateScript(Entity{ entity, this });
-					sc.Instance->OnCreate();
-				}
-			});
-		}
+	void Scene::OnSimulationStart()
+	{
+		OnPhysics2DStart();
 	}
 
 	void Scene::OnRuntimeStop()
 	{
-		{
-			// Scripts On Destroy
-			m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
-			{
-				if (sc.scriptInstatiated)
-				{
-					sc.Instance->OnDestroy();
-				}
-			});
-		}
+		// Scripts On Destroy
+		OnScriptsStop();
 
 		// Destroy Physics Objects
-		{
-			delete m_PhysicsWorld;
-			m_PhysicsWorld = nullptr;
-		}
+		OnPhysics2DStop();
+	}
+
+	void Scene::OnSimulationStop()
+	{
+		OnPhysics2DStop();
 	}
 
 	void Scene::OnUpdateRuntime(Timestep ts)
 	{
 		// Update scripts
-		{
-			m_Registry.view<NativeScriptComponent>().each([=](auto entity, auto& nsc)
-			{
-				if (!nsc.Instance)
-				{
-					nsc.Instance = nsc.InstantiateScript();
-					nsc.Instance->m_Entity = Entity{ entity, this };
-					nsc.Instance->OnCreate();
-				}
-				
-				nsc.Instance->OnUpdate(ts);
-			});
-
-			m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
-			{
-				if (!sc.scriptInstatiated)
-				{
-					sc.InstantiateScript(Entity{ entity, this });
-					sc.Instance->OnCreate();
-				}
-
-				sc.Instance->OnUpdate(ts);
-			});
-		}
+		OnScriptsUpdate(ts);
 
 		// Physics
-		{
-			m_PhysicsWorld->Step(ts, m_VelocityIteractions, m_PositionIteractions);
-
-			// Retrieve transform from box2d
-			auto view = m_Registry.view<Rigidbody2DComponent>();
-			for (auto e : view)
-			{
-				Entity entity = { e, this };
-				auto& transform = entity.GetComponent<TransformComponent>();
-				auto& rb2d = entity.GetComponent<Rigidbody2DComponent>();
-				
-				b2Body* body = (b2Body*)rb2d.RuntimeBody;
-				auto& position = body->GetPosition();
-				transform.Position.x = position.x;
-				transform.Position.y = position.y;
-				transform.Rotation.z = body->GetAngle();
-			}
-		}
+		OnPhysics2DUpdate(ts);
 		
 		// Render 2D
 		Camera* mainCamera = nullptr;
@@ -294,53 +179,28 @@ namespace Engine
 		{
 			Renderer2D::BeginScene(*mainCamera, cameraTransform);
 
-			{ // Draw Sprites
-				auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
-				for (auto entity : group)
-				{
-					auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
-
-					Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
-				}
-			}
-
-			{ // Draw Circles
-				auto view = m_Registry.view<TransformComponent, CircleRendererComponent>();
-				for (auto entity : view)
-				{
-					auto [transform, circle] = view.get<TransformComponent, CircleRendererComponent>(entity);
-
-					Renderer2D::DrawCircle(transform.GetTransform(), circle.Color, circle.Thickness, circle.Fade, (int)entity);
-				}
-			}
+			OnRender2DUpdate();
 
 			Renderer2D::EndScene();
 		}
 	}
 
+	void Scene::OnUpdateSimulation(Timestep ts, EditorCamera& camera)
+	{
+		OnPhysics2DUpdate(ts);
+
+		Renderer2D::BeginScene(camera);
+
+		OnRender2DUpdate();
+
+		Renderer2D::EndScene();
+	}
+
 	void Scene::OnUpdateEditor(Timestep ts, EditorCamera& camera)
 	{
 		Renderer2D::BeginScene(camera);
-		
-		{ // Draw Sprites
-			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
-			for (auto entity : group)
-			{
-				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
 
-				Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
-			}
-		}
-
-		{ // Draw Circles
-			auto view = m_Registry.view<TransformComponent, CircleRendererComponent>();
-			for (auto entity : view)
-			{
-				auto [transform, circle] = view.get<TransformComponent, CircleRendererComponent>(entity);
-
-				Renderer2D::DrawCircle(transform.GetTransform(), circle.Color, circle.Thickness, circle.Fade, (int)entity);
-			}
-		}
+		OnRender2DUpdate();
 
 		Renderer2D::EndScene();
 	}
@@ -387,6 +247,172 @@ namespace Engine
 		CopyComponentIfExists<Rigidbody2DComponent>(newEntity, entity);
 		CopyComponentIfExists<BoxCollider2DComponent>(newEntity, entity);
 		CopyComponentIfExists<CircleCollider2DComponent>(newEntity, entity);
+	}
+
+	void Scene::OnPhysics2DStart()
+	{
+		m_PhysicsWorld = new b2World({ 0.0f, -9.8f });
+
+		auto view = m_Registry.view<Rigidbody2DComponent>();
+		for (auto e : view)
+		{
+			Entity entity = { e, this };
+			auto& transform = entity.GetComponent<TransformComponent>();
+			auto& rb2d = entity.GetComponent<Rigidbody2DComponent>();
+
+			b2BodyDef bodyDef;
+			bodyDef.type = Rigidbody2DTypeToBox2DBodyType(rb2d.Type);
+			bodyDef.position.Set(transform.Position.x, transform.Position.y);
+			bodyDef.angle = transform.Rotation.z;
+
+			b2Body* body = m_PhysicsWorld->CreateBody(&bodyDef);
+
+			body->SetFixedRotation(rb2d.FixedRotation);
+			rb2d.RuntimeBody = body;
+
+			if (entity.HasComponent<BoxCollider2DComponent>())
+			{
+				auto& bc2d = entity.GetComponent<BoxCollider2DComponent>();
+
+				b2PolygonShape boxShape;
+				boxShape.SetAsBox(bc2d.Size.x * transform.Scale.x, bc2d.Size.y * transform.Scale.y, b2Vec2(bc2d.Offset.x, bc2d.Offset.y), 0);
+
+				b2FixtureDef fixtureDef;
+				fixtureDef.shape = &boxShape;
+				fixtureDef.density = bc2d.Density;
+				fixtureDef.friction = bc2d.Friction;
+				fixtureDef.restitution = bc2d.Restitution;
+				fixtureDef.restitutionThreshold = bc2d.RestitutionThreshold;
+
+				body->CreateFixture(&fixtureDef);
+			}
+
+			if (entity.HasComponent<CircleCollider2DComponent>())
+			{
+				auto& cc2d = entity.GetComponent<CircleCollider2DComponent>();
+
+				b2CircleShape circleShape;
+				circleShape.m_p.Set(cc2d.Offset.x, cc2d.Offset.y);
+				circleShape.m_radius = transform.Scale.x * cc2d.Radius;
+
+				b2FixtureDef fixtureDef;
+				fixtureDef.shape = &circleShape;
+				fixtureDef.density = cc2d.Density;
+				fixtureDef.friction = cc2d.Friction;
+				fixtureDef.restitution = cc2d.Restitution;
+				fixtureDef.restitutionThreshold = cc2d.RestitutionThreshold;
+
+				body->CreateFixture(&fixtureDef);
+			}
+		}
+	}
+
+	void Scene::OnScriptsStart()
+	{
+		m_Registry.view<NativeScriptComponent>().each([=](auto entity, auto& nsc)
+		{
+			if (!nsc.Instance)
+			{
+				nsc.Instance = nsc.InstantiateScript();
+				nsc.Instance->m_Entity = Entity{ entity, this };
+				nsc.Instance->OnCreate();
+			}
+		});
+
+		m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
+		{
+			if (!sc.scriptInstatiated)
+			{
+				sc.InstantiateScript(Entity{ entity, this });
+				sc.Instance->OnCreate();
+			}
+		});
+	}
+
+	void Scene::OnPhysics2DStop()
+	{
+		delete m_PhysicsWorld;
+		m_PhysicsWorld = nullptr;
+	}
+
+	void Scene::OnScriptsStop()
+	{
+		m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
+		{
+			if (sc.scriptInstatiated)
+			{
+				sc.Instance->OnDestroy();
+			}
+		});
+	}
+
+	void Scene::OnPhysics2DUpdate(Timestep ts)
+	{
+		m_PhysicsWorld->Step(ts, m_VelocityIteractions, m_PositionIteractions);
+
+		// Retrieve transform from box2d
+		auto view = m_Registry.view<Rigidbody2DComponent>();
+		for (auto e : view)
+		{
+			Entity entity = { e, this };
+			auto& transform = entity.GetComponent<TransformComponent>();
+			auto& rb2d = entity.GetComponent<Rigidbody2DComponent>();
+
+			b2Body* body = (b2Body*)rb2d.RuntimeBody;
+			auto& position = body->GetPosition();
+			transform.Position.x = position.x;
+			transform.Position.y = position.y;
+			transform.Rotation.z = body->GetAngle();
+		}
+	}
+
+	void Scene::OnScriptsUpdate(Timestep ts)
+	{
+		m_Registry.view<NativeScriptComponent>().each([=](auto entity, auto& nsc)
+		{
+			if (!nsc.Instance)
+			{
+				nsc.Instance = nsc.InstantiateScript();
+				nsc.Instance->m_Entity = Entity{ entity, this };
+				nsc.Instance->OnCreate();
+			}
+
+			nsc.Instance->OnUpdate(ts);
+		});
+
+		m_Registry.view<ScriptComponent>().each([=](auto entity, auto& sc)
+		{
+			if (!sc.scriptInstatiated)
+			{
+				sc.InstantiateScript(Entity{ entity, this });
+				sc.Instance->OnCreate();
+			}
+
+			sc.Instance->OnUpdate(ts);
+		});
+	}
+
+	void Scene::OnRender2DUpdate()
+	{
+		{ // Draw Sprites
+			auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
+			for (auto entity : group)
+			{
+				auto [transform, sprite] = group.get<TransformComponent, SpriteRendererComponent>(entity);
+
+				Renderer2D::DrawSprite(transform.GetTransform(), sprite, (int)entity);
+			}
+		}
+
+		{ // Draw Circles
+			auto view = m_Registry.view<TransformComponent, CircleRendererComponent>();
+			for (auto entity : view)
+			{
+				auto [transform, circle] = view.get<TransformComponent, CircleRendererComponent>(entity);
+
+				Renderer2D::DrawCircle(transform.GetTransform(), circle.Color, circle.Thickness, circle.Fade, (int)entity);
+			}
+		}
 	}
 
 	template <typename T>
