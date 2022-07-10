ec3053e15c08a7d96f01b592feed36f436106bd5
Steven Drovie <NightAngel47@users.noreply.github.com>
2022-07-09 21:33:01 -0500

    Set encoding style to Flow + use yaml-cpp to load files
    https://github.com/TheCherno/Hazel/pull/358

--- GameEngine/src/Engine/Scene/SceneSerializer.cpp
+++ GameEngine/src/Engine/Scene/SceneSerializer.cpp
@@ -16,6 +16,7 @@ namespace YAML
 			Node node;
 			node.push_back(rhs.x);
 			node.push_back(rhs.y);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 		
@@ -39,6 +40,7 @@ namespace YAML
 			node.push_back(rhs.x);
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 
@@ -64,6 +66,7 @@ namespace YAML
 			node.push_back(rhs.y);
 			node.push_back(rhs.z);
 			node.push_back(rhs.w);
+			node.SetStyle(EmitterStyle::Flow);
 			return node;
 		}
 		
@@ -328,11 +331,7 @@ namespace Engine
 
 	bool SceneSerializer::Deserialize(const std::string& filepath)
 	{
-		std::ifstream stream(filepath);
-		std::stringstream strStream;
-		strStream << stream.rdbuf();
-
-		YAML::Node data = YAML::Load(strStream.str());
+		YAML::Node data = YAML::LoadFile(filepath);
 		if (!data["Scene"])
 			return false;
 
