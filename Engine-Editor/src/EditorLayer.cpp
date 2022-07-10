5be8bf34f5a70b96cd7ecd9a549deb0c87a8e81a
Steven Drovie <NightAngel47@users.noreply.github.com>
2022-07-10 08:22:31 -0500

    Fixed Crash when checking m_HoveredEntity after it's ref was destroyed

--- Engine-Editor/src/EditorLayer.cpp
+++ Engine-Editor/src/EditorLayer.cpp
@@ -107,22 +107,6 @@ namespace Engine
 				break;
 		}
 
-		// Mouse picking
-		auto[mx, my] = ImGui::GetMousePos();
-		mx -= m_ViewportBounds[0].x;
-		my -= m_ViewportBounds[0].y;
-		glm::vec2 viewportSize = m_ViewportBounds[1] - m_ViewportBounds[0];
-		my = viewportSize.y - my;
-
-		int mouseX = (int)mx;
-		int mouseY = (int)my;
-
-		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
-		{
-			int pixelData = m_Framebuffer->ReadPixel(1, mouseX, mouseY);
-			m_HoveredEntity = pixelData == -1 ? Entity() : Entity((entt::entity)pixelData, m_ActiveScene.get());
-		}
-
 		// Overlay Rendering
 		OnOverlayRender();
 
@@ -626,6 +610,29 @@ namespace Engine
 			m_ActiveScene->DuplicateEntity(selectedEntity);
 		}
 
+	}
+
+	void EditorLayer::MousePicking()
+	{
+		// Mouse picking
+		auto [mx, my] = ImGui::GetMousePos();
+		mx -= m_ViewportBounds[0].x;
+		my -= m_ViewportBounds[0].y;
+		glm::vec2 viewportSize = m_ViewportBounds[1] - m_ViewportBounds[0];
+		my = viewportSize.y - my;
+
+		int mouseX = (int)mx;
+		int mouseY = (int)my;
+
+		if (mouseX >= 0 && mouseY >= 0 && mouseX < (int)viewportSize.x && mouseY < (int)viewportSize.y)
+		{
+			int pixelData = m_Framebuffer->ReadPixel(1, mouseX, mouseY);
+			m_HoveredEntity = pixelData == -1 ? Entity() : Entity((entt::entity)pixelData, m_ActiveScene.get());
+		}
+		else
+		{
+			m_HoveredEntity = Entity();
+		}
 	}
 
 	void EditorLayer::OnScenePlay()
